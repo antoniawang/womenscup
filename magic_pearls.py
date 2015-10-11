e6ce5c084af00374dc19c6dafdacf1fed257a4ef
Antonia Wang <antonia.t.wang@gmail.com>
2015-10-11 01:32:47 -0700

    modified q1 and q3

--- magic_pearls.py
+++ magic_pearls.py
@@ -1,29 +1,30 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def pearl(n_list):
-    sums = []
+    sums = 0
     while len(n_list) > 1:
         print "original list is", n_list
+        print "sum of original list is", sum(n_list)
         smallest = min(n_list)
         print "smallest is", smallest        
         n_list.remove(smallest)
         print "removed smallest, list is now", n_list
-        second_small = min(n_list)
+        second_small = min(n_list) 
         print "second smallest is", second_small
         n_list.remove(second_small)
         print "removed second smallest, list is now", n_list
-        restrung = smallest + second_small
-        "smallest + second_small =", restrung
+        restrung = (smallest + second_small) % ((10 ** 9) + 7)
+        print "smallest + second_small =", restrung
         n_list.append(restrung)
-        "Added the sum of two smallest to list, list is now", n_list
-        sums.append(restrung)
-        "sums list is", sums
+        print "Added the sum of two smallest to list, list is now", n_list
+        sums += restrung
+        print "sums is", sums
         print "\n"
         
     if len(n_list) == 1:
-        return int(sum(sums))
-    
-N = raw_input()
-N_string = raw_input().split()
-N_pearls = [float(n) for n in N_string]
+        return sums % ((10 ** 9) + 7)
+# N = raw_input()
+# N_string = raw_input().split()
+# N_pearls = [float(n) for n in N_string]
 
-print pearl(N_pearls)
+print pearl([2, 4, 1, 2, 10, 2, 3])
+print pearl([6, 5, 4, 1, 2])
--- possible_max.py
+++ possible_max.py
@@ -1,22 +1,38 @@
 # Problem Statement
 
-# You are given a set S = {1, 2, 3,…,N}. 
-#Find two integers A and B (A<B) from the set S such that the value of A & B is the maximum possible and less than the given integer K. 
-#In this case, & represents the operator bitwise AND.
-import itertools
-def find_max(n, k):
-    combos = tuple(itertools.combinations(xrange(1,n+1),2))
-    answers = [0]
-    for x, y in combos:
-    	if x < y:
-        	sumxy = (x & y)
-        	answers.append(sumxy)
-    answer = max([m for m in answers if m < k])
-    print answer   	
+# You are given a set S = {1, 2, 3, N}. 
+# Find two integers A and B (A<B) from the set S such that the value of A & B is the maximum possible and less than the given integer K. 
+# In this case, & represents the operator bitwise AND.
 
-T = int(raw_input())
-for _ in range(T):
-    S = raw_input().split()
-    num = int(S[0])
-    K = int(S[1])
-    find_max(num, K)
+def possible_max(n, k):
+    print n, k
+    answer = 0
+
+    if n > (2**k):
+        for i in range(k, n):
+            print "i is", i
+            bitand = i & n
+            print "bitand:", bitand
+            if answer < bitand < k:
+                answer = bitand
+    else:
+        if k % 2 == 1:
+            answer = k - 1
+        else:
+            answer = k - 2    
+
+    print "answer is"
+    return answer    
+
+       
+    
+# T = int(raw_input())
+# for _ in range(T):
+#     S = raw_input().split()
+#     N = int(S[0])
+#     K = int(S[1])
+#     print possible_max(N, K)
+
+print possible_max(5,2)
+print possible_max(8,5)
+print possible_max(2,2)
